e866f6ea0222dfaaf445937c88647d5d2b1b2cbf
IvanToth <ivan.tothba@gmail.com>
2018-03-07 20:15:03 +0100

    Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
     BrowserModule
